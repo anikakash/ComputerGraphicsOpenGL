e789777a0c94820d929103060c35e2137fe5fe67
anikakash <anik15-14210@diu.edu.bd>
2023-05-25 12:23:25 +0600

    adding files

--- May16/Task_1/main.cpp
+++ May16/Task_1/main.cpp
@@ -16,15 +16,10 @@ bool tri_fx=true, tri_fy=true;
 
 void myInit ()
 {
-
-    //glClearColor(0.0, 1.0, 0.0, 0.0);
-    //glClearColor(184.0f/255.0f, 213.0f/255.0f, 238.0f/255.0f, 1.0f);
-    //glColor3f(0.0, 0.2, 0.0);
     glClearColor(128.0f/255.0f,0, 0, 1.0f);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-
 
     // setting window dimension in X- and Y- direction
     glOrtho(0, 500, 0, 500, -10.0, 10.0);
@@ -59,95 +54,41 @@ void pac_man_mouth(){
     glVertex2f(250, 250);
     glVertex2f(150, tri_y);
     glEnd();
-//    if(tri_fx){
-//        tri_x--;
-//        if(tri_x==250)tri_fx=false;
-//    }
-//    else if(!tri_fx){
-//        tri_x++;
-//        if(tri_x==350)tri_fx=true;
-//    }
-//
-//    if(tri_fy){
-//        tri_y++;
-//        if(tri_y==250)tri_fy=false;
-//    }
-//    else if(!tri_fy){
-//        tri_y--;
-//        if(tri_y==150)tri_fy=true;
-//    }
+    if(tri_fx){
+        tri_x--;
+        if(tri_x==250)tri_fx=false;
+    }
+    else if(!tri_fx){
+        tri_x++;
+        if(tri_x==350)tri_fx=true;
+    }
+
+    if(tri_fy){
+        tri_y++;
+        if(tri_y==250)tri_fy=false;
+    }
+    else if(!tri_fy){
+        tri_y--;
+        if(tri_y==150)tri_fy=true;
+    }
 }
 
 void display ()
 {
-
-
-
     glClear(GL_COLOR_BUFFER_BIT);
-
-
-
-
-
     glBegin(GL_QUADS);
-//   glColor3f(173.0/255.0, 25.0/255.0, 9.0/255.0);
-    glColor3f(90.0/255.0, 252.0/255.0, 3.0/255.0);
+    glColor3f(173.0/255.0, 25.0/255.0, 9.0/255.0);
     glVertex2f( a1+0,b1+0 );
     glVertex2f( a1+500, b1+0 );
-//   glColor3f(173.0/255.0, 25.0/255.0, 9.0/255.0);
     glVertex2f( a2+500, b2+500);
     glVertex2f( a2+0, b2+500);
     glEnd();
-
-//    circle( x, y);
-//    glBegin(GL_TRIANGLES);
-//    glColor3f(3, 252, 252);
-//    glVertex2f( 250,500 );
-//    glVertex2f( 250, 0 );
-//    glEnd();
-
     circle( x, y);
     pac_man_mouth();
-
-//    tri_x--;
-//    printf("x = %d\n", tri_x); //250
-//    tri_y++;
-//    printf("y = %d\n", tri_y);
-//    if()printf("%d\n", tri_y); //150
-
-
-
-    /*
-
-
-
-    ghash(50,-30,61,61,61);
-    ghash(-100,-70,0,0,0);
-    grave(100,80);
-
-
-    glBegin(GL_TRIANGLE_FAN);
-    glColor3ub(64,29,50);
-
-    glVertex2f( 250, 250 );
-    glVertex2f( 0,100 );
-    glVertex2f( 0, 150 );
-    glVertex2f( 250, 140 );
-    glVertex2f( 350, 150 );
-    glVertex2f( 500, 160 );
-    glVertex2f( 500, 100 );
-    glEnd();
-    */
-
-
-
-
     glFlush();
 }
 void update(int val)
 {
-
-
     glutPostRedisplay();
     glutTimerFunc(20, update, 0);
 }
@@ -155,15 +96,10 @@ int main (int argc, char** argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-
-    // giving window size in X- and Y- direction
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 0);
-
-    // Giving name to window
-    glutCreateWindow("bahubali 3");
+    glutCreateWindow("Anik Dash Akash 201-15-14210");
     myInit();
-
     glutDisplayFunc(display);
     glutTimerFunc(25, update, 0);
     update(0);
---
+++ anik dash akash 201-15-14210 [Bounce Ball].cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include<windows.h>
+#include<stdio.h>
+#include<GL/glut.h>
+#include<math.h>
+#define pi 3.142857
+#include<iostream>
+using namespace std;
+
+int x=250;
+int y=250;
+int x_1=104;
+int y_1=104;
+int val=46, val2=592;
+bool right_side = true, left_side = false;
+int a1=0, b1=0, a2=0, b2=0;
+
+void myInit ()
+{
+
+    glClearColor(128.0f/255.0f,0, 0, 1.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    // setting window dimension in X- and Y- direction
+    glOrtho(0, 500, 0, 500, -10.0, 10.0);
+}
+void circle(float x,float y, float r)
+{
+    float x1,y1,x2,y2;
+    float radius=r;
+    x1=x;
+    y1=y;
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3ub(253,253,229);
+    glVertex2f(x1,y1);
+    float angle;
+    for (angle=0; angle<=360; angle+=0.5)
+    {
+        x2 = x1+sin((angle*3.1416)/180)*radius;
+        y2 = y1+cos((angle*3.1416)/180)*radius;
+        glVertex2f(x2,y2);
+    }
+
+    glEnd();
+
+
+}
+
+void display ()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_QUADS);
+
+    glColor3ub(96, 160, 233);
+
+    glVertex2f( a1+0,b1+0 );
+    glVertex2f( a1+500, b1+0 );
+
+    glColor3ub(159, 185, 248);
+    glVertex2f( a2+500, b2+500);
+    glVertex2f( a2+0, b2+500);
+    glEnd();
+
+
+//    x++;
+    if(y==446)
+    {
+        right_side = false;
+        left_side = true;
+    }
+    if(y==val){
+        right_side = true;
+        left_side = false;
+    }
+    if(right_side==true)
+    {
+        printf("%d %d\n",x, y);
+        y+=4;
+    }
+    else if(right_side==false ){
+         printf("%d %d\n",x, y);
+        y-=4;
+    }
+    circle( x, y, 50);
+//    circle(x_1, y_1, 40);
+    glFlush();
+}
+void update(int val)
+{
+    glutPostRedisplay();
+    glutTimerFunc(20, update, 0);
+}
+int main (int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+    // giving window size in X- and Y- direction
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(200, 100);
+
+    // Giving name to window
+    glutCreateWindow("Bounce Ball");
+    myInit();
+
+    glutDisplayFunc(display);
+    glutTimerFunc(25, update, 0);
+    update(0);
+    glutMainLoop();
+}
